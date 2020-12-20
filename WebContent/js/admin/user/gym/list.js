7f88a55003aea95b1586a83b7f82220d188d0e6b
nyarla12 <48872811+nyarla12@users.noreply.github.com>
2020-12-20 23:52:29 +0900

    checkbox

--- WebContent/js/admin/user/gym/list.js
+++ WebContent/js/admin/user/gym/list.js
@@ -1,13 +1,11 @@
 // gym
 window.addEventListener("load", function () {
-	var listBox = document.querySelector(".gym-list");
+	var listBox = document.querySelector(".gymList-box");
 	var checkAll = listBox.querySelector(".check-all");
 	var checks = listBox.querySelectorAll(".check")
-	// 위에껄 클릭하면 밑에 있는게 클릭 되야한다
-	// 어떻게 하면 위에껄 클릭해야 밑에께 클릭될까
-	// 어떻게 하면 좋을까 하나씩 해보자
-
+	
 	checkAll.onclick = function(){
+		//console.log("test");
 		if(checkAll.checked == true) {
 			for(var i=0;i<checks.length;i++)
 				checks[i].checked=true;
@@ -17,4 +15,5 @@ window.addEventListener("load", function () {
 				checks[i].checked=false;	
 		}
 	} 	
+
 });
